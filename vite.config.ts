c40cba77aa7bea2c28fb9a429b54f5a12e5a9438
Love Killander <lovekillander@Loves-MacBook-Air.local>
2026-02-20 22:09:03 +0100

    Fix Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-base: mode === "production" ? "/love-s-premium-portfolio/" : "/",
+base: "/",
 
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
